c60c2f58dd6989c6d1f6f41497d493a303a18246
Muntasir Udoy <muntasir.u@gmail.com>
2024-03-30 20:21:09 +0600

    feat : add footer

---
+++ src/app/widgets/footer/customer-service-support.tsx
@@ -0,0 +1,54 @@
+export default function customerServiceSupport() {
+  return (
+    <div className="font-caption mq450:gap-[36px_72px] flex h-[293px] flex-col items-start justify-start gap-[72px] text-left text-xl text-white">
+      <div className="flex flex-col items-start justify-start gap-[12px]">
+        <h3 className="font-inherit mq450:text-base mq450:leading-[24px] relative m-0 font-semibold leading-[150%] text-inherit">
+          USA Underwriter
+        </h3>
+        <div className="flex flex-row items-start justify-start text-sm">
+          <div className="relative leading-[150%]">
+            <p className="m-0">NAIC #: 30457</p>
+            <p className="m-0">P.O. Box 2426</p>
+            <p className="m-0">{`Rancho Cucamonga, CA, 91729 `}</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-1 flex-col items-start justify-start gap-[9px]">
+        <h3 className="font-inherit mq450:text-base mq450:leading-[24px] relative m-0 font-semibold leading-[150%] text-inherit">{`Customer Service & Support`}</h3>
+        <div className="flex flex-1 flex-col items-start justify-start gap-[7px] text-sm">
+          <div className="flex flex-1 flex-row items-center justify-start gap-[4px]">
+            <img
+              className="relative h-[15px] w-[15px] shrink-0 overflow-hidden object-cover"
+              loading="lazy"
+              alt=""
+              src="/heroiconsminiclock@2x.png"
+            />
+            <div className="relative whitespace-nowrap leading-[150%]">
+              Monday - Friday, 9:00 am - 6:00 pm ET
+            </div>
+          </div>
+          <div className="flex flex-1 flex-row items-center justify-start gap-[5px]">
+            <img
+              className="relative h-[15px] w-[15px] shrink-0 overflow-hidden"
+              alt=""
+              src="/heroiconssolidphone.svg"
+            />
+            <div className="relative inline-block min-w-[125px] leading-[150%]">
+              +1 (248) 541-2800
+            </div>
+          </div>
+          <div className="flex flex-1 flex-row items-center justify-center gap-[9px]">
+            <img
+              className="relative h-[15px] w-[15px] shrink-0 overflow-hidden"
+              alt=""
+              src="/heroiconsminienvelope.svg"
+            />
+            <div className="relative whitespace-nowrap leading-[150%]">
+              customerservice@usaunderwriters.com
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/widgets/footer/footer.tsx
+++ src/app/widgets/footer/footer.tsx
@@ -1,5 +1,37 @@
+import CustomerServiceSupport from "./customer-service-support";
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <>
+      <footer className="font-caption  relative z-[1] box-border flex max-w-full flex-col items-center justify-start gap-[90px] self-stretch overflow-hidden bg-gray-800 px-5 pb-[15px] pt-[74px] text-center text-sm text-gray-900">
+        <div className="container">
+          <div className="box-border flex w-full flex-row items-start justify-start py-0 pl-5 pr-[21px]">
+            <div className="mq1050:flex-wrap flex max-w-full flex-1 flex-row items-start justify-between gap-[20px]">
+              <CustomerServiceSupport />
+              <CustomerServiceSupport />
+              <CustomerServiceSupport />
+            </div>
+          </div>
+          <div className="z-[3] hidden h-[21px] w-0" />
+        </div>
+      </footer>
+      <footer className="flex w-full flex-row flex-wrap items-start justify-between gap-[20px] bg-white py-4">
+        <div className="container flex justify-between">
+          <div className="relative z-[1] inline-block max-w-full ">
+            <span>{`Copyright 2022 USA Underwriters `}</span>
+            <span className="font-medium">|</span>
+            <span>{` `}</span>
+            <span className="font-semibold">Privacy Policy | Terms of Use</span>
+          </div>
+          <div className="relative z-[1] inline-block  whitespace-nowrap">
+            <a className="text-[inherit]" href="https://www.neutrix.co/" target="_blank">
+              Neutrix
+            </a>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
 
 export default Footer;
